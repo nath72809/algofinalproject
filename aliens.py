37f74599f308e274a01849075f007b62064887df
Popka228 <104684936+Popka228@users.noreply.github.com>
2022-06-04 16:22:58 +0300

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -6,7 +6,23 @@ height = 600
 window = pygame.display.set_mode((width, height))
 clock = time.Clock()
 
+#управление
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx - 50, self.rect.top, 75, 60, -10)
+        bullets.add(bullet)
 
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+           self.kill()
 
 game = True
 
