5c0583669feb8acacfbbec00fd2f2e9fa21593ec
Popka228 <104684936+Popka228@users.noreply.github.com>
2022-06-11 16:12:58 +0300

    Add files via upload

---
+++ adopt.py
@@ -0,0 +1,36 @@
+import pygame
+pygame.init()
+
+width = 800
+height = 600
+window = pygame.display.set_mode((width, height))
+clock = time.Clock()
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx - 50, self.rect.top, 75, 60, -10)
+        bullets.add(bullet)
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+           self.kill()
+           global bullet_shot = False
+
+game = True
+
+ bullet_shot = False
+
+ while game != False:
+    pygame.display.update()
+    clock.tick(40)
+
+    if ent.key = Key_Space and bullet_shot == False:
+        player.fire()
