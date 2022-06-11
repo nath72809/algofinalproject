5a341231170f663c36f128745db6ccc9c1962cd9
artm <104684801+nath72809@users.noreply.github.com>
2022-06-11 16:24:30 +0300

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -6,6 +6,18 @@ height = 600
 window = pygame.display.set_mode((width, height))
 clock = time.Clock()
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, y, x, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (x, y))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ 
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+    
 #управление
 class Player(GameSprite):
     def update(self):
