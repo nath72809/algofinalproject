a4fd46dbaaffd81d40eab855f5c8a9b16a741559
artm <104684801+nath72809@users.noreply.github.com>
2022-06-11 16:18:04 +0300

    Update aliens.py

--- aliens.py
+++ aliens.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-
+direction = 0
 width = 800
 height = 600
 window = pygame.display.set_mode((width, height))
@@ -23,18 +23,22 @@ class Bullet(GameSprite):
         self.rect.y += self.speed
         if self.rect.y < 0:
            self.kill()
-           global bullet_shot = False
-
-game = True
-
-bullet_shot = False
-
-while game != False:
-    pygame.display.update()
-    clock.tick(40)
-
-    if ent.key = Key_Space and bullet_shot == False:
-        player.fire()
+        
+class Enemy(GameSprite):
+   def update(self):
+       if self.rect.x >= 50 and self.rect.x <= 450 and i == 0:
+            self.rect.x += 10
+       if self.rect.x >= 450:
+            self.rect.y += 25
+            i += 1
+       if self.rect.x <= 50:
+            self.rect.y += 25
+            i -= 1
+        if self.rect.x >= 50 and self.rect.x <= 450 and i == 1:
+            self.rect.x -= 10
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx - 50, self.rect.top, 75, 60, -10)
+        bullets.add(bullet)
         
 finish = False
 run = True
