64d8cba458743a921b64f0edf4d47d7c6a21bd50
Popka228 <104684936+Popka228@users.noreply.github.com>
2022-06-11 16:16:46 +0300

    Add files via upload

---
+++ aliens2.py
@@ -0,0 +1,53 @@
+import pygame
+pygame.init()
+
+width = 800
+height = 600
+window = pygame.display.set_mode((width, height))
+clock = time.Clock()
+
+#управление
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx - 50, self.rect.top, 75, 60, -10)
+        bullets.add(bullet)
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+            global bullet_shot = False
+
+game = True
+
+bullet_shot = False
+
+while game != False:
+    pygame.display.update()
+    clock.tick(40)
+
+    if ent.key = Key_Space and bullet_shot == False:
+        player.fire()
+        
+finish = False
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+ 
+    if not finish:
+        window.blit(background,(0,0))
+
+        Bullet.update()
